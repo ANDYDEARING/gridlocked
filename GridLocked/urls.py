9a6c9e9bb123a96f3f963b670bd3629e762e5c81
Andy Dearing <andy.dearing@gmail.com>
2019-09-20 14:19:13 -0400

    basic login page wired up

--- GridLocked/urls.py
+++ GridLocked/urls.py
@@ -19,10 +19,13 @@ from GridLocked import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/$', auth_views.LoginView.as_view(), name='login'),
+    path('logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 urlpatterns += [
