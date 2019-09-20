16efd7d4836596b26299e63b0170acb02ee8ad75
Andy Dearing <andy.dearing@gmail.com>
2019-09-20 13:22:08 -0400

    stores current page in the body tag

--- GridLocked/views.py
+++ GridLocked/views.py
@@ -3,20 +3,20 @@ from django.shortcuts import render, redirect
 def battles(request):
     """View function for battles page of site."""
     context={
-
+        'currentPage' : "battles",
     }
     return render(request, 'battles.html', context=context)
 
 def explore(request):
     """View function for explore page of site."""
     context={
-
+        'currentPage' : "explore",
     }
     return render(request, 'explore.html', context=context)
 
 def workshop(request):
     """View function for workshop page of site."""
     context={
-
+        'currentPage' : "workshop",
     }
     return render(request, 'workshop.html', context=context)
