d0064024fcad2ce41096c734ed378a912d987df4
Andy Dearing <andy.dearing@gmail.com>
2019-09-25 18:05:44 -0400

    refined notes

--- GridLocked/models.py
+++ GridLocked/models.py
@@ -19,10 +19,11 @@ class Fighter (models.Model):
     # and the same for a defense profile. Perhaps with a power factor of "1", the totals
     # of the profiles would be generated based on an average of "1". For example, given
     # a potential damage set of [lava,steel,energy,cold] an attack profile could be
-    # [1.2,0.8,0.5,1.5] and a defense profile could be [2,0.4,0.4,1.2]. Incoming and 
-    # outgoing damage would be multiplied by the corresponding value. These would have
-    # max bounds and randomized, increasing the chance of a player finding the perfect
-    # robot.
+    # [1.2, 0.8, 0.5, 1.5] and a defense profile could be [2.0, 0.4, 0.4, 1.2]. Incoming 
+    # and outgoing damage would be multiplied by the corresponding value. These would 
+    # have max bounds and randomized, making the opportunity for a player finding the 
+    # "perfect" robot for a goal or to match equipment. The table size needs to scale
+    # with the number of attributes.
 
     def __str__(self):
         return self.name
