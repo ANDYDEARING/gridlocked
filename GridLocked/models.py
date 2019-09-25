32b104bbf06c59971be8a9a1b4e7721d3a59b225
Andy Dearing <andy.dearing@gmail.com>
2019-09-25 14:04:43 -0400

    refined db relationships

--- GridLocked/models.py
+++ GridLocked/models.py
@@ -7,9 +7,9 @@ class Fighter (models.Model):
     name = models.CharField(max_length = 250)
     max_health = models.PositiveIntegerField(default=0)
     general = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    left_equip = models.ManyToManyField('Equipment', related_name='+')
-    right_equip = models.ManyToManyField('Equipment', related_name='+')
-    back_equip = models.ManyToManyField('Equipment', related_name='+')
+    left_equip = models.ForeignKey('Equipment', null=True, on_delete=models.SET_NULL, related_name='+')
+    right_equip = models.ForeignKey('Equipment', null=True, on_delete=models.SET_NULL, related_name='+')
+    back_equip = models.ForeignKey('Equipment', null=True, on_delete=models.SET_NULL, related_name='+')
 
     def __str__(self):
         return self.name
