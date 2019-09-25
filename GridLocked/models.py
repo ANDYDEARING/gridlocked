f50c103f907b1ca6bbf892507e818d1dba1e8490
Andy Dearing <andy.dearing@gmail.com>
2019-09-25 18:01:32 -0400

    added design notes for attack and defense profiles

--- GridLocked/models.py
+++ GridLocked/models.py
@@ -13,6 +13,16 @@ class Fighter (models.Model):
     # maybe this needs to be stored as a defensive profile will all attributes and a multiplier (rails seeds)
     tough_vs = models.ManyToManyField(to='Attribute', blank=True, related_name='+')
     weak_vs = models.ManyToManyField(to='Attribute', blank=True, related_name='+')
+    # also might need an attack profile
+
+    # ideally there would be a mechanism for a procedurally generated attack profile
+    # and the same for a defense profile. Perhaps with a power factor of "1", the totals
+    # of the profiles would be generated based on an average of "1". For example, given
+    # a potential damage set of [lava,steel,energy,cold] an attack profile could be
+    # [1.2,0.8,0.5,1.5] and a defense profile could be [2,0.4,0.4,1.2]. Incoming and 
+    # outgoing damage would be multiplied by the corresponding value. These would have
+    # max bounds and randomized, increasing the chance of a player finding the perfect
+    # robot.
 
     def __str__(self):
         return self.name
