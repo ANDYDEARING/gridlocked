1f98766dae42560a98c7e89d3b8e8015c2dcc783
Andy Dearing <andy.dearing@gmail.com>
2019-09-27 12:02:59 -0400

    refactored models for equipment and fighters

--- GridLocked/models.py
+++ GridLocked/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
 from django.urls import reverse
+from django.core.validators import MinValueValidator
+from decimal import *
 
 class Fighter (models.Model):
     name = models.CharField(max_length = 250)
@@ -10,23 +12,13 @@ class Fighter (models.Model):
     left_equip = models.ForeignKey('Equipment', null=True, on_delete=models.SET_NULL, related_name='+')
     right_equip = models.ForeignKey('Equipment', null=True, on_delete=models.SET_NULL, related_name='+')
     back_equip = models.ForeignKey('Equipment', null=True, on_delete=models.SET_NULL, related_name='+')
-    # this needs to be stored as a defensive profile will all attributes and a multiplier (rails seeds)
-    tough_vs = models.ManyToManyField(to='Attribute', blank=True, related_name='+')
-    weak_vs = models.ManyToManyField(to='Attribute', blank=True, related_name='+')
-    # also might need an attack profile
-
-    # ideally there would be a mechanism for a procedurally generated attack profile
-    # and the same for a defense profile. Perhaps with a power factor of "1", the totals
-    # of the profiles would be generated based on an average of "1". For example, given
-    # a potential damage set of [lava,steel,energy,cold] an attack profile could be
-    # [1.2, 0.8, 0.5, 1.5] and a defense profile could be [2.0, 0.4, 0.4, 1.2]. Incoming 
-    # and outgoing damage would be multiplied by the corresponding value. These would 
-    # have max bounds and randomized, making the opportunity for a player finding the 
-    # "perfect" robot for a goal or to match equipment. The table size needs to scale
-    # with the number of attributes.
+    energy_value = models.DecimalField(max_digits=2, decimal_places=1, default=0.0, validators=[MinValueValidator(Decimal(0.0))])
+    acid_value = models.DecimalField(max_digits=2, decimal_places=1, default=0.0, validators=[MinValueValidator(Decimal(0.0))])
+    metal_value = models.DecimalField(max_digits=2, decimal_places=1, default=0.0, validators=[MinValueValidator(Decimal(0.0))])
+    quantum_value = models.DecimalField(max_digits=2, decimal_places=1, default=0.0, validators=[MinValueValidator(Decimal(0.0))])
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - E:{energy_value} A:{acid_value} M:{metal_value} Q:{quantum_value}"
 
     class Meta:
         ordering = ['name']
@@ -35,7 +27,13 @@ class Equipment (models.Model):
     name = models.CharField(max_length = 250)
     weapon_range = models.PositiveIntegerField(default=0)
     force = models.IntegerField(default=0)
-    attribute = models.ForeignKey(to='Attribute', on_delete=models.SET_NULL, null=True, blank=True)
+    ATTRIBUTE_CHOICES = [
+        ('E', 'ENERGY'),
+        ('A', 'ACID'),
+        ('M', 'METAL'),
+        ('Q', 'QUANTUM')
+    ]
+    attribute = models.CharField(max_length=1, choices= ATTRIBUTE_CHOICES, default='E')
     min_range_offest = models.PositiveIntegerField(default=0)
     area_of_effect = models.PositiveIntegerField(default=0)
 
@@ -44,11 +42,3 @@ class Equipment (models.Model):
 
     class Meta:
         verbose_name_plural = 'Equipment'
-
-class Attribute (models.Model):
-    name = models.CharField(max_length = 250)
-
-    def __str__(self):
-        return self.name
-
-
