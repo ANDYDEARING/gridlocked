2d995f3b09d0331764fe062945e29535d75f21ab
Andy Dearing <andy.dearing@gmail.com>
2019-10-07 16:53:41 -0400

    removed test

--- generator.py
+++ generator.py
@@ -1,6 +1,10 @@
 from random import randint
 
 def next_number(total_so_far, numbers_remaining, set_length):
+    """
+    Generates the next elemental effect number on a random mech. This assumes the
+    average value across all stats is 3 out of 5
+    """
     target_number = (3 * set_length) - total_so_far
     if target_number < 1:
         print("You messed up")
@@ -9,11 +13,6 @@ def next_number(total_so_far, numbers_remaining, set_length):
         min_value = max(target_number-(5*(numbers_remaining-1)) ,1)
         max_value = min(target_number-(1*(numbers_remaining-1)) ,5)
         return randint(min_value,max_value)
-def test():
-    i = 0
-    while i<10:
-        print(next_number(245,50,100))
-        i = i+1
 
 
 
