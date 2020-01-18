c174fffb84c0ad3855a026851a9c3e525c3a7fdd
Andy Dearing <andy.dearing@gmail.com>
2020-01-18 16:17:20 -0500

    switched django-heroku install syntax

--- GridLocked/settings.py
+++ GridLocked/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 # STATIC_TMP = os.path.join(BASE_DIR, 'static')
 # os.makedirs(STATIC_TMP, exist_ok=True)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
