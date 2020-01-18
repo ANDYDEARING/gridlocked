31a9f71d71908d6c415a6914ff975e9724e12efe
Andy Dearing <andy.dearing@gmail.com>
2020-01-18 17:25:29 -0500

    trying to get heroku to rebuild

--- GridLocked/settings.py
+++ GridLocked/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+
+# make heroku reload
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
